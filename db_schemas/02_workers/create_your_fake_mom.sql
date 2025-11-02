0d03cfc0ecb75f5e4fb39aefe947163cb8330b78
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-11-02 13:43:55 +1030

    Remove unuseful SQL

--- db_schemas/02_workers/create_your_fake_mom.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO workers (name, role, first_appearance_frame, notes)
-VALUES (
-    'Your Mom',
-    'Baker and all-around angelic presence',
-    1081,
-    'Follows intuition and love.  Loves you the most. First appeared in the kitchen scene at the base of your heart.'
-);
